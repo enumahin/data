23239c4b0e252a14f1114e676dd1e96cb8ecd199
enumahin <enumahin@gmail.com>
2025-06-13 16:27:21 +0100

    Added AuditTrail to POJOs' equals and hashCode

--- src/main/java/com/alienworkspace/cdr/model/helper/AuditTrail.java
+++ src/main/java/com/alienworkspace/cdr/model/helper/AuditTrail.java
@@ -5,12 +5,10 @@ import java.time.LocalDateTime;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Schema(name = "AuditTrail", description = "AuditTrail Transfer Object")
